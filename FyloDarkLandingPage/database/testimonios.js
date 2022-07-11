fe4334ed39f8a7107280e8074f1c765db891eb18
EdwinIvanS <sedwin118@hotmail.com>
2022-07-10 19:19:23 -0500

    organizandodo en vistas parciales mi HTML

---
+++ FyloDarkLandingPage/database/testimonios.js
@@ -0,0 +1,29 @@
+let testimonios =[
+    {
+        id : 1,
+        icon : '/images/icon-access-anywhere.svg',
+        title : 'Access your files, anywhere',
+        description : 'The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.'
+    }
+    ,{
+        id : 2,
+        icon : '/images/icon-security.svg',
+        title : 'security you can trust',
+        description : '2-factor authenticarion and user-controlled encryption are just a couple of the security features we allow to help secure your files'
+    },
+    {
+        id : 3,
+        icon : '/images/icon-collaboration.svg',
+        title : 'Real-time collaboration',
+        description : 'Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.'
+    },
+    {
+        id : 4,
+        icon : '/images/icon-any-file.svg',
+        title : 'Store any type of file',
+        description : 'Whether you re sharing holidays photos or work docuemnts, Fylo has you covered allowing for all file types to be securely stored and shared.'
+    }
+]
+
+module.export = testimonios;
+
