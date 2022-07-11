5fe183f15c02f046960c6e5898e7c62d1ac60cdb
EdwinIvanS <sedwin118@hotmail.com>
2022-07-10 20:12:41 -0500

    vistas renderizadas

--- FyloDarkLandingPage/controllers/mainController.js
+++ FyloDarkLandingPage/controllers/mainController.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const descripcionesFilePath = path.join(__dirname, '../database/descripcionesBody.json');
+const descripciones = JSON.parse(fs.readFileSync(descripcionesFilePath, 'utf-8'));
+
+const testimoniosFilePath = path.join(__dirname, '../database/testimonios.json');
+const testimonios = JSON.parse(fs.readFileSync(testimoniosFilePath, 'utf-8'));
 
 const mainController= {
     index : function(req, res){
-        res.render('index');
+        console.log(testimonios);
+        res.render('index', {testimonios ,descripciones });
     }
 }
 
